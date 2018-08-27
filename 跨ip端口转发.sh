d27002c4fc3bbd70a8a76481a767720138ccad07
吴彰涛 <1182517419@qq.com>
2018-08-27 11:33:03 +0800

    Update 跨ip端口转发.sh

--- 跨ip端口转发.sh
+++ 跨ip端口转发.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 端口转发
 pro='tcp'
-NAT_Host='172.31.17.223'
+NAT_Host='****'
 NAT_Port=9100
-Dst_Host='172.31.17.2'
+Dst_Host='****'
 Dst_Port=9100
 iptables -t nat -A PREROUTING  -m $pro -p $pro --dport $NAT_Port -j DNAT --to-destination $Dst_Host:$Dst_Port
 iptables -t nat -A POSTROUTING -m $pro -p $pro --dport $Dst_Port -d $Dst_Host -j SNAT --to-source $NAT_Host
